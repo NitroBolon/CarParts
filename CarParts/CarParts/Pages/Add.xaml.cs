d57a61b60cf5a3f439cf12225def1d2014367b9b
Janek M <miljanek14@gmail.com>
2021-06-12 20:31:03 +0200

    added new features cruD

--- CarParts/CarParts/Pages/Add.xaml.cs
+++ CarParts/CarParts/Pages/Add.xaml.cs
@@ -2,19 +2,9 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CarParts.Pages
 {
@@ -42,7 +32,6 @@ namespace CarParts.Pages
         private void RefreshLists()
         {
             Ccar.ItemsSource = wnd.Cars.Find(new BsonDocument()).ToListAsync().Result;
-            Ccar.DisplayMemberPath = "model";
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
--- CarParts/CarParts/Pages/Delete.xaml.cs
+++ CarParts/CarParts/Pages/Delete.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CarParts.DataModels;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CarParts.Pages
 {
@@ -20,12 +13,23 @@ namespace CarParts.Pages
     /// </summary>
     public partial class Delete : Page
     {
-        Window wnd;
+        MainWindow wnd;
 
         public Delete()
         {
             InitializeComponent();
             wnd = (MainWindow)App.Current.MainWindow;
+
+            RefreshLists();
+        }
+
+        private void RefreshLists()
+        {
+            Wmake.ItemsSource = wnd.Wheels.Find(new BsonDocument()).ToListAsync().Result;
+            Emake.ItemsSource = wnd.EngineParts.Find(new BsonDocument()).ToListAsync().Result;
+            Smake.ItemsSource = wnd.SuspensionParts.Find(new BsonDocument()).ToListAsync().Result;
+            Cmake.ItemsSource = wnd.Cars.Find(new BsonDocument()).ToListAsync().Result;
+            Cname.ItemsSource = wnd.Clients.Find(new BsonDocument()).ToListAsync().Result;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -37,6 +41,101 @@ namespace CarParts.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             wnd.Title = "CarParts: Usuń";
+        }
+
+        private void Wheel_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (Wheel)Wmake.SelectedItem;
+            
+            if(item == null)
+            {
+                MessageBox.Show("Wybierz obiekt do usunięcia", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var deleteFilter = Builders<Wheel>.Filter.Eq("_id", item._id);
+
+            wnd.Wheels.DeleteOne(deleteFilter);
+
+            MessageBox.Show("Obiekt usunięty z bazy", "CarParts: Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RefreshLists();
+        }
+
+        private void Engine_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (Engine)Emake.SelectedItem;
+
+            if (item == null)
+            {
+                MessageBox.Show("Wybierz obiekt do usunięcia", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var deleteFilter = Builders<Engine>.Filter.Eq("_id", item._id);
+
+            wnd.EngineParts.DeleteOne(deleteFilter);
+
+            MessageBox.Show("Obiekt usunięty z bazy", "CarParts: Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RefreshLists();
+        }
+
+        private void Suspension_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (Suspension)Smake.SelectedItem;
+
+            if (item == null)
+            {
+                MessageBox.Show("Wybierz obiekt do usunięcia", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var deleteFilter = Builders<Suspension>.Filter.Eq("_id", item._id);
+
+            wnd.SuspensionParts.DeleteOne(deleteFilter);
+
+            MessageBox.Show("Obiekt usunięty z bazy", "CarParts: Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RefreshLists();
+        }
+
+        private void Client_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (Client)Cname.SelectedItem;
+
+            if (item == null)
+            {
+                MessageBox.Show("Wybierz obiekt do usunięcia", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var deleteFilter = Builders<Client>.Filter.Eq("_id", item._id);
+
+            wnd.Clients.DeleteOne(deleteFilter);
+
+            MessageBox.Show("Obiekt usunięty z bazy", "CarParts: Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RefreshLists();
+        }
+
+        private void Car_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (Car)Cmake.SelectedItem;
+
+            if (item == null)
+            {
+                MessageBox.Show("Wybierz obiekt do usunięcia", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var deleteFilter = Builders<Car>.Filter.Eq("_id", item._id);
+
+            wnd.Cars.DeleteOne(deleteFilter);
+
+            MessageBox.Show("Obiekt usunięty z bazy", "CarParts: Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            RefreshLists();
         }
     }
 }
