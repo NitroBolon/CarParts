e1b37238b808c1dc54ddf69257bbd51d5007cd88
Janek M <miljanek14@gmail.com>
2021-06-13 00:20:29 +0200

    added one more feature

--- CarParts/CarParts/Pages/Clients.xaml.cs
+++ CarParts/CarParts/Pages/Clients.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CarParts.DataModels;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,12 +23,15 @@ namespace CarParts.Pages
     /// </summary>
     public partial class Clients : Page
     {
-        Window wnd;
+        MainWindow wnd;
 
         public Clients()
         {
             InitializeComponent();
             wnd = (MainWindow)App.Current.MainWindow;
+
+            Cli.ItemsSource = wnd.Clients.Find(new BsonDocument()).ToListAsync().Result;
+            Car.ItemsSource = wnd.Cars.Find(new BsonDocument()).ToListAsync().Result;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -37,6 +43,81 @@ namespace CarParts.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             wnd.Title = "CarParts: Propozycje";
+        }
+
+        private void Cli_Click(object sender, RoutedEventArgs e)
+        {
+            var res = "";
+            var item = (Client)Cli.SelectedItem;
+            if(item != null)
+            {
+                var cars = item.car.Split(',');
+
+                foreach(var x in cars)
+                {
+                    var carFilter = Builders<Car>.Filter.Eq("carId", x);
+                    var car = wnd.Cars.Find(carFilter).FirstOrDefault();
+                    res += $"Części do {car.name} {car.model}:\n";
+                    res += $"{CarProducts(car.vin)}\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano obiektu", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            resCli.Text = res;
+        }
+
+        private void Car_Click(object sender, RoutedEventArgs e)
+        {
+            var res = "";
+            var item = (Car)Car.SelectedItem;
+            if (item != null)
+            {
+                var vin = item.vin;
+
+                res += CarProducts(vin);
+            }
+            else
+            {
+                MessageBox.Show("Nie wybrano obiektu", "CarParts: Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            resCar.Text = res;
+        }
+
+        private string CarProducts(string vin)
+        {
+            string res = "";
+
+            var wheelFilter = Builders<Wheel>.Filter.Regex("vin", new BsonRegularExpression($".*{vin}.*"));
+            var susFilter = Builders<Suspension>.Filter.Regex("vin", new BsonRegularExpression($".*{vin}.*"));
+            var engFilter = Builders<Engine>.Filter.Regex("vin", new BsonRegularExpression($".*{vin}.*"));
+
+            var wheels = wnd.Wheels.Find(wheelFilter).ToList();
+            var suspensions = wnd.SuspensionParts.Find(susFilter).ToList();
+            var engines = wnd.EngineParts.Find(engFilter).ToList();
+
+            res += "Felgi:\n";
+            foreach(var x in wheels)
+            {
+                res += $"{x.name} {x.model} - {x.diameter}\" - {x.price}PLN\n";
+            }
+
+            res += "\nCzęści zawieszenia:\n";
+            foreach (var x in suspensions)
+            {
+                res += $"{x.name} {x.model} - {x.purpose} - {x.price}PLN\n";
+            }
+
+            res += "\nCzęści do silnika:\n";
+            foreach (var x in engines)
+            {
+                res += $"{x.name} {x.model} - {x.purpose} - {x.price}PLN\n";
+            }
+
+            return res;
         }
     }
 }
